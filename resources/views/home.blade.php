42bfd80b41158a6f71de39832b773c912095740c
esraq <esraq26552@gmail.com>
2024-12-01 00:29:45 +0600

    1st commit "mastering admin & template"

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.master')
+
+@section('content')
+
+
+<div id="page-inner">
+<div class="row">
+                    <div class="col-md-12">
+                     <h2>Super Admin Dashboard</h2>
+                        <h5>Welcome {{ Auth::user()->name }} , Love to see you back. </h5>
+                    </div>
+                </div>
+                 <!-- /. ROW  -->
+                  <hr />
+
+			</div>
+
+
+
+
+
+
+@endsection
